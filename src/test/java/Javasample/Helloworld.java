0c0f21a41406f3f5dd692d08640d85dbbd698cc7
bevara.mohanrao@gmail.com <77225451+bevara-mohanrao@users.noreply.github.com>
2023-11-20 17:13:09 -0500

    This is new sample

---
+++ src/test/java/Javasample/Helloworld.java
@@ -0,0 +1,11 @@
+package Javasample;
+
+public class Helloworld {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		System.out.println("Hello World this is Maven project with Tomcast testing");
+	}
+
+}
